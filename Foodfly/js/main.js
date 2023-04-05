1fd0cd4009e758856ac596a9ad6fea4cb69f005d
krutheesh <krutheeshsiripuram@gmail.com>
2023-04-05 22:04:23 +0530

    projects

---
+++ Foodfly/js/main.js
@@ -0,0 +1,16 @@
+/* =========== Show Navbar =========== */
+const navbar = document.querySelector(".navbar");
+const hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+});
+
+
+
+/* =========== Scroll Position =========== */
+
+/* =========== Smooth Scroll =========== */
+
+/* =========== Preloader =========== */
+
+/* =========== Scroll Top =========== */
